c4566ebffa21a0bc9090e60cae9e62d6ca774fdf
Luhao Yao <Luhao.Yao@partner.bmwgroup.com>
2021-01-20 17:54:26 +0800

    finish 26 remove deplicates from sorted array

---
+++ geek_java/template/_26_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,18 @@
+package template;
+
+import java.util.Arrays;
+
+public class _26_Remove_Duplicates_from_Sorted_Array {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 2};
+        System.out.println(removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+        nums = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        return 0;
+    }
+}
